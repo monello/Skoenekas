4ec5c6d72cf9c0cd87add0aded7521c7cde67af6
Morne Louw <louw.morne@gmail.com>
2013-06-15 18:24:43 +0200

    Updated the uninstall SQL (0.2.06)
    Updated the uninstall SQL
    Updated the new design mockups

--- com_dnagifts/admin/sql/uninstall.mysql.utf8.sql
+++ com_dnagifts/admin/sql/uninstall.mysql.utf8.sql
@@ -1,23 +1,29 @@
-DROP TABLE IF EXISTS `#__dnagifts_user_activity`;
-DROP TABLE IF EXISTS `#__dnagifts_lst_action`;
-DROP TABLE IF EXISTS `#__dnagifts_institution`;
-DROP TABLE IF EXISTS `#__dnagifts_lst_institution_type`;
-DROP TABLE IF EXISTS `#__dnagifts_lnk_user_institution`;
-DROP TABLE IF EXISTS `#__dnagifts_test`;
-DROP TABLE IF EXISTS `#__dnagifts_lst_gift`;
-DROP TABLE IF EXISTS `#__dnagifts_question`;
-DROP TABLE IF EXISTS `#__dnagifts_option_button`;
-DROP TABLE IF EXISTS `#__dnagifts_lnk_test_buttonset`;
-DROP TABLE IF EXISTS `#__dnagifts_lnk_test_question`;
-DROP TABLE IF EXISTS `#__dnagifts_lst_user_type`;
-DROP TABLE IF EXISTS `#__dnagifts_lnk_user_tests`;
-DROP TABLE IF EXISTS `#__dnagifts_lnk_user_test_answers`;
-DROP TABLE IF EXISTS `#__dnagifts_lnk_user_gifts`;
-DROP TABLE IF EXISTS `#__dnagifts_pretest_info`;
-DROP TABLE IF EXISTS `#__dnagifts_lst_countries`;
-DROP TABLE IF EXISTS `#__dnagifts_lst_language_codes`;
-
+DROP VIEW IF EXISTS `#__dnagifts_churchlist`;
+DROP VIEW IF EXISTS `#__dnagifts_citylist`;
 DROP VIEW IF EXISTS `#__dnagifts_count_testquestions`;
 DROP VIEW IF EXISTS `#__dnagifts_list_testbuttons`;
 DROP VIEW IF EXISTS `#__dnagifts_list_testquestions`;
 DROP VIEW IF EXISTS `#__dnagifts_testquestions_and_answers`;
+DROP VIEW IF EXISTS `#__dnagifts_pastorlist`;
+
+DROP TABLE IF EXISTS `#__dnagifts_lnk_test_buttonset`;
+DROP TABLE IF EXISTS `#__dnagifts_lnk_test_question`;
+DROP TABLE IF EXISTS `#__dnagifts_lnk_user_tests`;
+DROP TABLE IF EXISTS `#__dnagifts_lnk_user_test_answers`;
+DROP TABLE IF EXISTS `#__dnagifts_lnk_user_gifts`;
+DROP TABLE IF EXISTS `#__dnagifts_lnk_user_institution`;
+
+DROP TABLE IF EXISTS `#__dnagifts_pretest_info`;
+DROP TABLE IF EXISTS `#__dnagifts_guages_svg`;
+DROP TABLE IF EXISTS `#__dnagifts_user_activity`;
+DROP TABLE IF EXISTS `#__dnagifts_institution`;
+DROP TABLE IF EXISTS `#__dnagifts_test`;
+DROP TABLE IF EXISTS `#__dnagifts_question`;
+DROP TABLE IF EXISTS `#__dnagifts_option_button`;
+
+DROP TABLE IF EXISTS `#__dnagifts_lst_institution_type`;
+DROP TABLE IF EXISTS `#__dnagifts_lst_gift`;
+DROP TABLE IF EXISTS `#__dnagifts_lst_user_type`;
+DROP TABLE IF EXISTS `#__dnagifts_lst_countries`;
+DROP TABLE IF EXISTS `#__dnagifts_lst_language_codes`;
+DROP TABLE IF EXISTS `#__dnagifts_lst_action`;
