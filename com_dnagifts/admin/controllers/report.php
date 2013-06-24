c9f8bfcf4e6a0d31130af52542bea3bd2403f23d
Morne Louw <louw.morne@gmail.com>
2013-06-24 19:58:33 +0200

    initial code for admin reporting (0.2.08)

---
+++ com_dnagifts/admin/controllers/report.php
@@ -0,0 +1,14 @@
+<?php
+// No direct access to this file
+defined('_JEXEC') or die('Restricted access');
+ 
+// import Joomla controlleradmin library
+jimport('joomla.application.component.controlleradmin');
+ 
+/**
+ * Report Controller
+ */
+class DnaGiftsControllerReport extends JControllerAdmin
+{
+	protected $default_view = 'reports';
+}
